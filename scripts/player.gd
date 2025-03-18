90954c21afdc46e7bbaf92a70c298d793fab4f1f
Matt Pahayo <mpahayo@gmail.com>
2025-03-18 05:49:54 -0500

    redo movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,27 +2,22 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-var move_left: bool = false
-var move_right: bool = false
-var move_up: bool = false
-var move_down: bool = false
-
-var last_input = null
-var last_pos = null
-
 var move_input := Vector2i.ZERO
 
 func _physics_process(delta):
 	
 	var direction = Vector2i.ZERO
-	direction.x = (Input.get_axis("move_left", "move_right"))
-	direction.y = (Input.get_axis("move_up", "move_down"))
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
 	
 	if direction.x != 0:
 		move_input = Vector2(direction.x, 0)
 	elif direction.y != 0:
 		move_input = Vector2(0, direction.y)
 	
-	velocity = SPEED * move_input	
-	move_and_slide()
-	print(move_input)
+	#velocity = SPEED * move_input	
+	#move_and_slide()
+	
+	# move with dedicated tiles
+	# create map and tiles first
+	
