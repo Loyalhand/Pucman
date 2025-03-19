2e3fb80e2e88a64289c2f7695af4dd438b833fa3
Matt Pahayo <mpahayo@gmail.com>
2025-03-19 18:00:29 -0500

    Sprites

--- scripts/player.gd
+++ scripts/player.gd
@@ -35,16 +35,16 @@ func get_input():
 	
 	if Input.is_action_pressed("move_left"):
 		next_movement_direction = Vector2.LEFT
-		rotation_degrees = 270
+		rotation_degrees = 180
 	elif  Input.is_action_pressed("move_right"):
 		next_movement_direction = Vector2.RIGHT
-		rotation_degrees = 90
+		rotation_degrees = 0
 	elif Input.is_action_pressed("move_down"):
 		next_movement_direction = Vector2.DOWN
-		rotation_degrees = 180
+		rotation_degrees = 90
 	elif Input.is_action_pressed("move_up"):
 		next_movement_direction = Vector2.UP
-		rotation_degrees = 0
+		rotation_degrees = 270
 
 # creates a raycast of its collision shape in the direction of next input and 
 # returns true if no collidables are in the shape
